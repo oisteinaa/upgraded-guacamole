295da2db2cdc4365e432a69bdc89785a41ded68d
Oistein Aanensen <oistein.aanensen@inapril.com>
2025-04-16 08:22:01 +0200

    test

--- src/Capture/capture_zmq.py
+++ src/Capture/capture_zmq.py
@@ -77,6 +77,7 @@ def process_data(file):
 
 	# Include the shape of the data in the payload
 	data = f['data'][:, ::10]
+	data.astype(np.int16)
 	# serialized = msgpack.packb(data.tolist())
 	# compressed = compress_data(data, compression_level=22)
 	payload = {
