84624f97aab32810bf5d8d88d8198c547b579203
Oistein Aanensen <oistein.aanensen@inapril.com>
2025-05-05 16:11:20 +0200

    sensitivity error

--- src/Capture/capture_zmq.py
+++ src/Capture/capture_zmq.py
@@ -74,7 +74,7 @@ def process_data(file, mastdf):
     data = np.multiply(data, f['header']['dataScale'][()])
     data = unwrap(data, f['header']['spatialUnwrRange'][()],axis=1)
     data = np.cumsum(data,axis=0)*f['header']['dt']
-    data /=f['header']['sensitivity']
+    data /=f['header']['sensitivities'][0]
     
     with ThreadPoolExecutor() as executor:
         rms_future = executor.submit(get_rms, data)
