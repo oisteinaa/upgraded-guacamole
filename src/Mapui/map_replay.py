7ae82baf0b5ec9c63229e4cffa3b8a4f992dbe72
Oistein Aanensen <oistein.aanensen@inapril.com>
2025-11-10 22:38:47 +0100

    test

--- src/Mapui/map_replay.py
+++ src/Mapui/map_replay.py
@@ -16,7 +16,7 @@ from sensnetlib.dbfunc import get_mastliste
 BASE_URL = "http://127.0.0.1:5000"
 
 
-def main(app, date="20251110", frame_interval=10):
+def main(app, date="20251110", frame_interval=60):
     geom = get_mastliste()
 
     app.layout = html.Div([
